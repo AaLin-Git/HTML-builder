1fce1d6999f62475ef91e0c02764018e25c05208
AaLin-Git <aalin.box.it@gmail.com>
2023-05-08 20:27:28 +0200

    feat: resolve 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const sourcePath = path.join(__dirname, 'files');
+const copyPath = path.join(__dirname, 'files-copy');
 
+fs.mkdir(copyPath, { recursive: true }, err => {
+  if (err) {
+    throw err;
+  }
+});
+
+fs.readdir(sourcePath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        fs.copyFile(path.join(sourcePath, file.name), path.join(copyPath, file.name), err => {
+          if (err) {
+            throw err;
+          }
+        });
+      }
+    })
+  }
+})
