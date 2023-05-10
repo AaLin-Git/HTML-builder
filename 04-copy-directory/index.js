b44a2a210da33d148f41dd6a82a2e4cab61195f1
AaLin-Git <aalin.box.it@gmail.com>
2023-05-10 19:48:59 +0200

    fix: resolve task correctly

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,26 +1,36 @@
 const fs = require('fs');
+const rimraf = require("rimraf");
 const path = require('path');
 const sourcePath = path.join(__dirname, 'files');
 const copyPath = path.join(__dirname, 'files-copy');
 
-fs.mkdir(copyPath, { recursive: true }, err => {
+rimraf(copyPath, (err) => {
   if (err) {
     throw err;
   }
+  copyDir();
 });
 
-fs.readdir(sourcePath, { withFileTypes: true }, (err, files) => {
-  if (err) {
-    console.log(err);
-  } else {
-    files.forEach((file) => {
-      if (file.isFile()) {
-        fs.copyFile(path.join(sourcePath, file.name), path.join(copyPath, file.name), err => {
-          if (err) {
-            throw err;
-          }
-        });
-      }
-    });
-  }
-});
+const copyDir = () => {
+  fs.mkdir(copyPath, { recursive: true }, err => {
+    if (err) {
+      throw err;
+    }
+  });
+
+  fs.readdir(sourcePath, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach((file) => {
+        if (file.isFile()) {
+          fs.copyFile(path.join(sourcePath, file.name), path.join(copyPath, file.name), err => {
+            if (err) {
+              throw err;
+            }
+          });
+        }
+      });
+    }
+  });
+}
